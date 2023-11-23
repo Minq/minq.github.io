4dad7fd094a527abe968abae918f16f59541880f
minq <minq1205@gmail.com>
2023-11-23 23:22:04 +0900

    refactor: https://github.com/ixartz/Next-js-Blog-Boilerplate 템플릿을 사용하도록

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,9 @@
+export const AppConfig = {
+  site_name: 'minq blog',
+  title: 'minq 블로그',
+  description: 'I never dreamed about success. I worked for it.',
+  url: 'https://minq.me',
+  locale: 'ko',
+  author: 'minq',
+  pagination_size: 32,
+};
